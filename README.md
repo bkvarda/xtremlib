2a85500ecfccc20b79de917ad153dfd599255fcf
bkvarda <bjkvarda@gmail.com>
2014-10-22 22:43:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<snippet>
+  <content><![CDATA[
+# ${1:xtremlib}
+ 
+xtremlib is a PowerShell Module that acts as a wrapper for interactions with the XtremIO RESTful API
+This is currently incomplete, I intend to include most API functionality as well as make content more presentable
+ 
+## Installation
+ 
+TODO: Instructions to come...
+ 
+## Usage
+ 
+TODO: Usage to come...
+ 
+ 
+## History
+ 
+Started today!
+ 
+
+]]></content>
+  <tabTrigger>readme</tabTrigger>
+</snippet>
---
+++ xtremlib.ps1
@@ -0,0 +1,51 @@
+﻿<#
+xtremlib is a PowerShell Module that acts as a wrapper for interactions with the XtremIO RESTful API
+This is currently incomplete, I intend to include most API functionality as well as make content more presentable
+
+#TODO
+ -Lots
+ -Implement token-based security
+ -Implement all basic storage creation/setting commands
+ -Implement snapshot commands
+
+Written by : Brandon Kvarda
+             @bjkvarda
+             
+
+#>
+
+
+#Returns XtremIO Cluster Name
+Function Get-XtremClusterName ([string]$xioname,[string]$username,[string]$password)
+{
+  $header = Get-XtremAuthHeader -username $username -password $password
+  $format = @{Expression={$._clusters};Label="System Name"}
+
+  (Invoke-RestMethod -Uri https://$xioname/api/json/types/clusters -Headers $header -Method Get).clusters
+
+}
+
+#Returns Various XtremIO Statistics
+Function Get-XtremClusterStatus ([string]$xioname,[string]$username,[string]$password)
+{
+  $header = Get-XtremAuthHeader -username $username -password $password
+  $uri = "https://"+$xioname+"/api/json/types/clusters/?name="+$xioname
+
+  (Invoke-RestMethod -Uri $uri -Headers $header -Method Get).content                                   
+
+}
+
+
+#Generates Header to be used in requests to XtremIO
+Function Get-XtremAuthHeader([string]$username,[string]$password)
+{
+  
+  $basicAuth = ("{0}:{1}" -f $username,$password)
+  $basicAuth = [System.Text.Encoding]::UTF8.GetBytes($basicAuth)
+  $basicAuth = [System.Convert]::ToBase64String($basicAuth)
+  $headers = @{Authorization=("Basic {0}" -f $basicAuth)}
+
+  return $headers
+
+}
+
