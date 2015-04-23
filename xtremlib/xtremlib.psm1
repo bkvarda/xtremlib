e8fb831bf15dbe29fdd0ca6d3d4f5b0d9d5c0dc4
bkvarda <bjkvarda@gmail.com>
2015-04-22 21:21:52 -0700

    Added cert bypass

--- xtremlib/xtremlib.psm1
+++ xtremlib/xtremlib.psm1
@@ -13,6 +13,20 @@ Written by : Brandon Kvarda
 
 #>
 
+#########DISABLE CERT VALIDATION####
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
 ######### GLOBAL VARIABLES #########
 $global:XtremUsername =$null
 $global:XtremPassword =$null
